15bf4e501acae51e2f719b0c95d9de1416bdd958
jigang.duan <djg4055108@126.com>
2019-07-03 15:54:02 +0800

    modify config

--- src/config/config.prod.ts
+++ src/config/config.prod.ts
@@ -5,7 +5,7 @@ module.exports = (appInfo: any) => {
     host: 'localhost',
     port: '3306',
     user: 'root',
-    password: 'root',
+    password: '123456',
     database: 'vlog',
     dialect: 'mysql',
   };
